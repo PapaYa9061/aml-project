51f7802236b7e8746687eda1ec6e78a0e15ee03d
f-egers <81555308+f-egers@users.noreply.github.com>
2021-09-20 21:39:56 +0200

    sim. parameters can now change dynamically

--- src/simulation.py
+++ src/simulation.py
@@ -23,13 +23,22 @@ def build_random_adj_matrix(size, D, plot_deg_dist=False):
     return connection_matrix
 
 
+default_counter_measure_parameters= {
+    "soc_dist_threshold": 1,
+    "soc_dist_effect": 0.5,
+    "sanitary_measures_threshold": 1,
+    "sanitary_measures_effect": 0.5,
+    "lifting_threshold": 0.5
+}
+
+
 class Simulation:
 
-    def __init__(self, pop_size, D, base_transmission_prob, time_infected):
+    def __init__(self, pop_size, D, base_transmission_prob, time_infected, counter_measure_params= default_counter_measure_parameters):
 
         # network properties
         self.pop_size = pop_size
-        self.D = D
+        self.base_D = D
 
         # Disease properties
         self.base_transmission_prob = base_transmission_prob
@@ -37,6 +46,11 @@ class Simulation:
 
         self.connection_matrix = build_random_adj_matrix(
             self.pop_size, D)  # adj. matrix for the network
+        self.social_dist_mask = np.random.rand(self.pop_size,self.pop_size) > counter_measure_params["soc_dist_effect"]
+
+        self.counter_measure_params = counter_measure_params
+        self.soc_dist_factor = 1
+        self.sanitary_factor = 1
 
         # indicates if a node is susceptible (1) or not (0)
         self.is_suscep = np.ones(self.pop_size)
@@ -55,16 +69,26 @@ class Simulation:
         self.infected_for[idx] = self.time_infected
 
     def transmission_prob(self):
-        '''potentially model countermeasures'''
-        return self.base_transmission_prob
 
-    def step(self, n=1, disable_progress_bar=True, steps_after_halt=1):
+        return self.sanitary_factor*self.base_transmission_prob
+
+    def D(self):
+        D = self.soc_dist_factor*self.base_D
+        return int(2 * (D <= 2) + (D > 2) * D)
+
+    def step(self, n=1, disable_progress_bar=True, steps_after_halt=2):
         '''perform n steps of the simulation'''
         halt_counter = 0
         for i in tqdm(range(n), desc="running simulation", disable=disable_progress_bar):
             self.record()
 
-            pot_infections = (((self.connection_matrix)@self.is_infected)*self.is_suscep).astype(int)
+            self.dynamic_parameter_adjustment()
+
+            eff_connection_matrix = np.array(self.connection_matrix)
+            if self.D() != self.base_D:
+                eff_connection_matrix = eff_connection_matrix * self.social_dist_mask
+
+            pot_infections = (((eff_connection_matrix)@self.is_infected)*self.is_suscep).astype(int)
             infections = np.zeros(self.pop_size)
             for i in np.where(pot_infections != 0)[0]:
                 if (np.random.rand(pot_infections[i]) < self.transmission_prob()).any():
@@ -93,8 +117,8 @@ class Simulation:
                 np.sum(self.is_infected == 1),
                 np.sum((self.is_infected == 0) & (self.is_suscep == 0)),
                 #self.pop_size,
-                self.D,
-                self.base_transmission_prob,
+                self.D(),
+                self.transmission_prob(),
                 self.time_infected]
 
     def reset(self):
@@ -112,19 +136,53 @@ class Simulation:
         self.infected_for = np.zeros(self.pop_size)
         self.infected_for[self.is_infected == 1] = np.random.randint(1, self.time_infected+1, np.sum(self.is_infected))
 
+    def get_number_of_new_cases(self):
+
+        if len(self.time_series) >= 2:
+            change_in_infected = self.time_series[-1][1]-self.time_series[-2][1]
+            change_in_recovered = self.time_series[-1][2]-self.time_series[-2][2]
+            return change_in_infected+change_in_recovered
+        else:
+            return 0
+
+    def dynamic_parameter_adjustment(self):
+
+        incidence = self.get_number_of_new_cases()/self.pop_size
+        if incidence > self.counter_measure_params["soc_dist_threshold"]:
+            self.soc_dist_factor = self.counter_measure_params["soc_dist_effect"]
+        elif incidence < self.counter_measure_params["lifting_threshold"] * self.counter_measure_params["soc_dist_threshold"]:
+            self.soc_dist_factor = 1
+
+        if incidence > self.counter_measure_params["sanitary_measures_threshold"]:
+            self.sanitary_factor = self.counter_measure_params["sanitary_measures_effect"]
+        elif incidence < self.counter_measure_params["lifting_threshold"] * self.counter_measure_params["sanitary_measures_threshold"]:
+            self.sanitary_factor = 1
+
+
+
+
+
 
 if __name__ == "__main__":
 
-    pop_size = 1000
-    D = 10
-    trans_prob = 0.1
-    days_infected = 6
+    pop_size = 2000
+    D = 9
+    trans_prob = 0.05
+    days_infected = 14
+    
+    counter_measure_parameters= {
+        "soc_dist_threshold": 0.03,
+        "soc_dist_effect": 0.2,
+        "sanitary_measures_threshold": 0.03,
+        "sanitary_measures_effect": 0.2,
+        "lifting_threshold": 0.1
+    }
 
     build_random_adj_matrix(pop_size, D, True)
 
-    sim = Simulation(pop_size, D, trans_prob, days_infected)
-    sim.infect_random(5)
-    sim.step(50)
+    sim = Simulation(pop_size, D, trans_prob, days_infected, counter_measure_params=counter_measure_parameters)
+    sim.infect_random(3)
+    sim.step(200)
 
     plt.plot(sim.time_series)
     plt.show()
